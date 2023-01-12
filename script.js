0a7c2d008aa4435ca5cfe9e6417259c882b6e4c9
Great-kiola <hollabodey.kilaso@gmail.com>
2023-01-12 00:51:00 +0000

    Updated the javascript code

--- script.js
+++ script.js
@@ -89,56 +89,75 @@ var upperCasedCharacters = [
 ];
 
 
+// var allChar = [].concat(upperCasedCharacters, lowerCasedCharacters, numericCharacters, specialCharacters); // New array
 
-var allChar = [].concat(upperCasedCharacters, lowerCasedCharacters, numericCharacters, specialCharacters); // New array
+var passwordLength = 9; 
+var selectedArr = [];
+
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  var characNum = prompt('How many characters do you want your password to contain?');
-  
-  let passwordLength = characNum
-  if (characNum >= 10 && characNum <=64 ) {
-    console.log(passwordLength);
-  } else {
-    alert('Value must be greater than 16 and less than 35')
+  selectedArr = [];
+
+  passwordLength = parseInt(prompt('How many characters do you want your password to contain? between 10 - 34 characters'));
+  if (isNaN(passwordLength) || passwordLength < 10 || passwordLength > 64 ) {
+    alert ("Value must be greater than 16 and less than 35. Please try again");
+    return false
   }
-
-
-  var specialCharac = confirm('Click OK to include special characters');
-  var numericCharac = confirm('Click OK to include numeric characters');
-  var lowercaseCharac = confirm('Click OK to include lowercase characters');
-  var uppercaseCharac= confirm('Click OK to include Uppercase characters');
+  
+  if (confirm('Click OK to include special characters')) {
+    selectedArr = selectedArr.concat(specialCharacters);
+  }
+  if (confirm('Click OK to include numeric characters')) {
+    selectedArr = selectedArr.concat(numericCharacters);
+  }
+  if (confirm('Click OK to include lowercase characters')) {
+    selectedArr = selectedArr.concat(lowerCasedCharacters);
+  }
+  if (confirm('Click OK to include Uppercase characters')) {
+    selectedArr = selectedArr.concat(upperCasedCharacters);
+  }
+  return true;
 
 }
   
   
 
 // Function for getting a random element from an array
-function getRandom(arr, num) {
-  const shuffled = [...arr].sort(() => 0.5 - Math.random());
-  return shuffled.slice(0, num).join("")
-
+function getRandom(arr) {
+  var password = "";
+  for (let i = 0; i< passwordLength; i++) {
+    var arr = Math.floor(Math.random() * selectedArr.length);
+    password = password + selectedArr[arr];
+  }
+  return password;
 }
 
-const arr = allChar
 
 
 
 // Function to generate password with user input
 function generatePassword() {
-  getPasswordOptions();
-  return getRandom(arr, 10);
+ return getRandom();
 }
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var correctValue = getPasswordOptions();
   var passwordText = document.querySelector('#password');
 
-  passwordText.value = password;
+  if (correctValue) {
+    var newPassword = generatePassword();
+    
+    passwordText.value = newPassword;
+  } else {
+    passwordText.value = "";
+  }
+
 }
 
 // Add event listener to generate button
@@ -151,40 +170,4 @@ generateBtn.addEventListener('click', writePassword);
 
 
 
-
-// if (characNum >= 16 && characNum <=65 ) {
-//   alert('valid input')
-// } else {
-//   alert('Value must be greater than 16 and less than 35')
-// }
-
-
-// var allChar = [].concat(upperCasedCharacters, lowerCasedCharacters, numericCharacters, specialCharacters);
-
-//   for (let i = 0; i < allChar; 1++){
-//   let randomNumber = Math.floor(Math.random() * allChar);
-    
-//   ); 
-//   console.log(randomNumber);
-
-//   password += allChar.substring(randomNumber, randomNumber + 1);
-//   }
-
-
-// console.log(characNum);
-// console.log(specialCharac);
-// console.log(numericCharac);
-// console.log(lowercaseCharac);
-// console.log(uppercaseCharac);
-
-
-
-
-// for (i= 0; i = arr.length; i++){
-//   let randomNumber = Math.floor(Math.random()* arr.length);
-//   const item =arr[randomNumber];
-//   return item;
-// }
-// const result = getRandom(allChar, 16);
-// console.log(result);
 
